c8971792e260f726d66c228af301d13b22cd981d
Kanishk Singh Maurya <95866249+kanishk466@users.noreply.github.com>
2026-01-06 12:50:05 +0530

    Delete src directory

--- src/models/User.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({}, { strict: false });
-
-const User = mongoose.model("User", UserSchema, "users");
-
-export default User;
